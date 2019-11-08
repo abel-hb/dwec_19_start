043e2436a29f5cd26460bd8645927e28be8aa368
Abel Herrero <abelherrero.17@campuscamara.es>
2019-11-08 03:50:00 +0100

    [PRUEBA CODE] PROYECTO

--- Mi Proyecto JS/js/proyecto.js
+++ Mi Proyecto JS/js/proyecto.js
@@ -20,7 +20,7 @@ function validar() {
 	for(let i = 0; i < objPersona.length; i++) {
 		// Chequeamos el usuario y la contraseña en el objPersona que es una array
 		if(username == objPersona[i].username && password == objPersona[i].password) {
-			window.location="login.html";
+			window.location="index.html";
       alert("Login Correcto")
       return
   		}
@@ -28,3 +28,68 @@ function validar() {
   	alert("Login Incorrecto, Introduce el usuario y contraseña correcta")
 
 }
+
+var Bus = true;
+var Uber = true;
+var Coche = true;
+/*var Lisa = true;
+var Maggie= true;*/
+
+function $(selector){
+    return document.querySelector(selector);
+}
+
+$("#bus").addEventListener("click", function(){
+var list=document.getElementById("pB");
+if(Bus){
+    list.style.display="block";
+    Bus=false;
+}else{
+    list.style.display="none";
+    Bus=true;
+}
+});
+
+$("#uber").addEventListener("click", function(){
+    var list=document.getElementById("pU");
+    if(Uber){
+        list.style.display="block";
+        Uber=false;
+    }else{
+        list.style.display="none";
+        Uber=true;
+    }
+});
+
+$("#coche").addEventListener("click", function(){
+    var list=document.getElementById("pC");
+    if(Coche){
+        list.style.display="block";
+        Coche=false;
+    }else{
+        list.style.display="none";
+        Coche=true;
+    }
+});
+/*
+$("#l").addEventListener("click", function(){
+        var list=document.getElementById("parrafoL");
+        if(Lisa){
+            list.style.display="block";
+            Lisa=false;
+        }else{
+            list.style.display="none";
+            Lisa=true;
+        }
+});
+
+$("#ma").addEventListener("click", function(){
+    var list=document.getElementById("parrafoMA");
+    if(Maggie){
+        list.style.display="block";
+        Maggie=false;
+    }else{
+        list.style.display="none";
+        Maggie=true;
+    }
+	});*/
